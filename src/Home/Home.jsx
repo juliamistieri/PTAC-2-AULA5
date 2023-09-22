62f2b740da0c42ce39fc16b806f4c75ad54dd998
juliamistieri <julia.mistieri02@gmail.com>
2023-09-22 08:55:49 -0400

    cod certo
    cod certo

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import './style.css';
+import toyotaMarca from './toyotaMarca.png'
+
 export default function Home() {
-   
-    return (
-        <div>
+    const [name, setName] = useState(0);
+    return(
+        <div className="casaEstilo">
             <h1>Home</h1>
-            <Link to="/todo">todo</Link>
+            <Link to="/todo">Vai para Home</Link>
+            <img src={toyotaMarca} alt="imagem" width="100" height="50"></img>
+            <p>{name}</p>
+            <input type="text" onChange={(e)=>{setName(e.target.value)}}/>
         </div>
 
-    );
-}
+    )};
--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,13 +1,59 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
-export default function ToDo() {
-   
-    return (
-        <div>
-            <h1>Lista de Atividades</h1>
+export default function Todo() {
+    const [lista, setLista] = useState([]);
+    const [idCriar, setIdCriar] = useState(1);
+    const [carros, setCarros] = useState("");
+    const [marca, setMarca] = useState("");
+
+    const salvar = (e) => {
+        e.preventDefault();
+        setLista([...lista, {
+            id:idCriar,
+            carros:carros,
+            marca:marca
+        }]);
+        setCarros("")
+        setMarca("")
+        setIdCriar(idCriar + 1)
+    };
+
+    const remover = (id) => {
+        const auxLista = [];
+        lista.map((carros) => {
+            if (carros.id !== id) {
+                auxLista.push(carros);
+            }
+        });
+        setLista(auxLista);
+    }
+    return(
+        <div className="relampago">
             <Link to="/">home</Link>
-           
+            <h1>Lista de Carros</h1>
+            <img src={supra.jpg} alt="imagem" width="100" height="50"></img>
+            <form onSubmit={salvar}>
+                <input type="text"
+                value={carros}
+                onChange={(e)=>
+                {setCarros(e.target.value)}} />
+                <input type="text"
+                value={marca}
+                onChange={(e)=>
+                {setMarca(e.target.value)}} />
+                <button>ADICIONAR</button>
+            </form>
+            {lista.map((carros)=>
+            <ul key={carros.id}>
+                <li>
+                <p>{carros.carros}</p>
+                <p>{carros.marca}</p>
+                <button onClick={() => remover(carros.id)}>REMOVER</button>
+                </li>
+            </ul>
+            )};
         </div>
     );
 }
